118d0bc78fadee81ba29ab3e5e9be813e9d7d51f
mponiente <michaelwrites.biz@gmail.com>
2026-05-01 01:06:47 -0700

    styled stuff

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,15 +7,17 @@ import Admin from "./pages/Admin"
 import Navbar from "./components/layout/Navbar"
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/workshop" element={<Events />} />
-        <Route path="/admin" element={<Admin />} />
-      </Routes>
-    </BrowserRouter>
-  )
+    return (
+        <div className="font-teachers">
+            <BrowserRouter>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/workshop" element={<Events />} />
+                    <Route path="/admin" element={<Admin />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    )
 }
