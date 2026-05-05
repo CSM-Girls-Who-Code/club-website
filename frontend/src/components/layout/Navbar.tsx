66e4fa5e409f82bb72bac951516676138aa52e05
Natalie Luong <natalieluong1818@gmail.com>
2026-05-05 14:40:26 -0700

    Added background nav

--- frontend/src/components/layout/Navbar.tsx
+++ frontend/src/components/layout/Navbar.tsx
@@ -1,6 +1,6 @@
 export default function Navbar() {
     return (
-        <nav className="w-full flex items-center justify-between px-4">
+        <nav className="w-full flex items-center justify-between px-4 background-amaranth">
             <div className="">
                 <h1 className="text-4xl font-bold">
                     Girls Who Code
